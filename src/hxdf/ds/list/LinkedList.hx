d0e1799006e3e5dcf1fda04ec74dda966a41ca16
Marika <48089643+Marika-0@users.noreply.github.com>
2019-09-06 09:31:10 +0930

    Changed to use absolute reference

--- src/hxdf/ds/list/LinkedList.hx
+++ src/hxdf/ds/list/LinkedList.hx
@@ -3,4 +3,4 @@ package hxdf.ds.list;
 /**
     An alias for `hxdf.ds.list.SingleLinkedList`.
 **/
-typedef LinkedList<T> = SingleLinkedList<T>;
+typedef LinkedList<T> = hxdf.ds.list.SingleLinkedList<T>;
