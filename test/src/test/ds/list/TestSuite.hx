25fc047da6f6c0a077a84d71d7ec0050aafb4640
Marika <48089643+Marika-0@users.noreply.github.com>
2019-09-21 14:58:19 +0930

    Added test cases.

--- test/src/test/ds/list/TestSuite.hx
+++ test/src/test/ds/list/TestSuite.hx
@@ -3,5 +3,8 @@ package test.ds.list;
 class TestSuite extends TestBroker {
     public function new() {
         addTest(test.ds.list.AssociativeListTests);
+        addTest(test.ds.list.DoubleLinkedListTests);
+        addTest(test.ds.list.SelfOrganizingListTests);
+        addTest(test.ds.list.SingleLinkedListTests);
     }
 }
