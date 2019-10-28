de4d57d1316d884f529a325a2ea8066a1894d105
Marika <48089643+Marika-0@users.noreply.github.com>
2019-10-28 21:58:21 +1030

    Added tests for hxdf.ds.Array

--- test/src/test/ds/TestSuite.hx
+++ test/src/test/ds/TestSuite.hx
@@ -2,6 +2,7 @@ package test.ds;
 
 class TestSuite extends TestBroker {
     public function new() {
+        addTest(test.ds.ArrayTests);
         addBroker(test.ds.list.TestSuite);
         addBroker(test.ds.tuple.TestSuite);
         addBroker(test.ds.unit.TestSuite);
