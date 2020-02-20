bdc7a3e794530c0120c139aae06d08438176a9bf
Marika <48089643+Marika-0@users.noreply.github.com>
2020-02-21 01:08:29 +1030

    Fixed iterators so they actually make sense

--- src/hxdf/lambda/Iterator.hx
+++ src/hxdf/lambda/Iterator.hx
@@ -3,63 +3,44 @@ package hxdf.lambda;
 import hxdf.ds.unit.KeyValuePair;
 
 /**
-    Typedef of `InputIterator<T>`.
+    Alias for `InputIterator<T>`.
 **/
 typedef Iterator<T> = InputIterator<T>;
 
 /**
-    An iterator that can retrieve data and be copied.
+    An iterator operating on a series of elements.
 **/
 interface InputIterator<T> {
     /**
-        Tells if the InputIterator can retrieve another value.
+        Returns `true` if the iterator can be advanced or `false` otherwise.
+
+        This function is not required to be called before a call to `next()`.
     **/
     function hasNext():Bool;
 
     /**
-        Increments the InputIterator and returns the value being iterated over.
+        Returns the current value of the iterator and advances to the next one.
+
+        If this function is called while `hasNext()` returns `false`, the result
+        may be unspecified.
     **/
     function next():T;
 
     /**
-        Creates a copy of the InputIterator with the same focus and returns it.
-
-        Copies of certain InputIterators (eg when reading a file) may be
-        redundant. In this case, the function is a no-op.
+        Creates a copy of the InputIterator over the same iterable and at the
+        same position.
     **/
     function copy():InputIterator<T>;
 }
 
 /**
-    An iterator that can recall the value returned by `next` arbitrarily.
+    An iterator operating over a defined sequence of elements.
 **/
-interface ExtractableIterator<T> {
+interface SequentialIterator<T> extends InputIterator<T> {
     /**
-        Returns the current value being iterated over (the value returned by
-        `next()`) without incrementing iteration.
-
-        If `next()` has not yet been called, returns the value that the first
-        call to `next()` will return.
-    **/
-    function value():T;
-}
-
-/**
-    An iterator that operates over a defined sequence of elements.
-**/
-interface SequentialIterator<T> {
-    /**
-        Advances the SequentialIterator `steps` units and returns the last value
-        iterated over.
-
-        If `steps` is greater than the remaining number of elements available to
-        iterator over or is negative, the result may be unspecified. See
-        specific implementations for details.
-    **/
-    function advance(steps:Int):T;
-
-    /**
-        Tests the equity of the SequentialIterator with `it`.
+        Tests the equity of the SequentialIterator with `it`, returning `true`
+        if the SequentialIterator and `it` are at the same position of
+        iteration or `false` otherwise.
 
         If the SequentialIterator and `it` are not of the same type, or not
         iterating over the same object, the result is unspecified.
@@ -67,83 +48,45 @@ interface SequentialIterator<T> {
     function equals(it:SequentialIterator<T>):Bool;
 }
 
-/**
-    An iterator over pairs of keys and their corresponding values.
-**/
-interface KeyValueIterator<K, V> extends InputIterator<KeyValuePair<K, V>> {
+interface IndexIterator<T> extends InputIterator<KeyValuePair<Int, T>> {
     /**
-        Returns the current key being iterated over.
+        Compares the IndexIterator with another IndexIterator `it`.
 
-        If `next()` has not yet been called, returns the value that the first
-        call to `next().key` will return.
+        If the IndexIterator is before `it`, the result is negative. If `it` is
+        before the IndexIterator the result is position. If the IndexIterator
+        and `it` have the same position, the result is `0`.
+
+        If the IndexIterator and `it` are not of the same type, or not iterating
+        over the same object, the result is unspecified.
     **/
-    function key():K;
-
-    /**
-        Returns the current value being iterated over.
-
-        If `next()` has not yet been called, returns the value that the first
-        call to `next().value` will return.
-    **/
-    function value():V;
-
-    /**
-        Returns the current key-value pair being iterated over (the value
-        returned by `next`) without incrementing the KeyValueIterator.
-
-        If `next()` has not yet been called, returns the value that the first
-        call to `next()` will return.
-    **/
-    function pair():KeyValuePair<K, V>;
+    function compare(it:IndexIterator<T>):Int;
 }
 
 /**
-    An iterator that can be advanced an arbitrary number of steps, arbitrarily
-    retrieve iterated values, and undergo equity comparison.
+    An iterator operating over a defined sequence of elements that can move in
+    reverse.
 **/
-interface UnidirectionalIterator<T> extends ExtractableIterator<T> extends SequentialIterator<T> {}
-
-/**
-    An iterator over key-value pairs of indexes and values of a defined sequence
-    of elements.
-**/
-interface IndexIterator<T> extends SequentialIterator<KeyValuePair<Int, T>> extends KeyValueIterator<Int, T> {
+interface BidirectionalIterator<T> extends SequentialIterator<T> {
     /**
-        The current position of the IndexIterator.
+        Returns `true` if the iterator can be retreated or `false` otherwise.
 
-        Equivalent to a call to `key()`.
-    **/
-    function position():Int;
-}
-
-/**
-    An iterator that can be advanced or retreated an arbitrary number of steps.
-**/
-interface BidirectionalIterator<T> extends UnidirectionalIterator<T> {
-    /**
-        Tells if the BidirectionalIterator can be decremented.
+        This function is not required to be called before a call to `prev()`.
     **/
     function hasPrev():Bool;
 
     /**
-        Decrements the BidirectionalIterator and returns the value being
-        iterated over.
+        Returns the current value of the iterator and retreats to the previous
+        one.
+
+        If this function is called while `hasPrev()` returns `false`, the result
+        may be unspecified.
     **/
     function prev():T;
-
-    /**
-        Retreats the BidirectionalIterator `steps` units and returns the last
-        value iterated over.
-
-        If `steps` is greater than the preceding number of elements available to
-        iterator over or is negative, the result may be unspecified. See
-        specific implementations for details.
-    **/
-    function retreat(steps:Int):T;
 }
 
 /**
-    An iterator that can be set to and retrieve values from arbitrary positions.
+    An iterator that can be set to and retrieve values from arbitrary positions
+    in a defined sequence of elements.
 **/
 interface RandomAccessIterator<T> extends BidirectionalIterator<T> {
     /**
@@ -154,19 +97,10 @@ interface RandomAccessIterator<T> extends BidirectionalIterator<T> {
     /**
         Sets the RandomAccessIterator to `position`.
 
-        If `position` is outside the bounds of iteration, the result may be
-        unspecified. See specific implementations for details.
+        If `pos` is outside the bounds of iteration, the result may be
+        unspecified.
     **/
-    function setToPosition(index:Int):T;
-
-    /**
-        Returns the value `offset` units from the RandomAccessIterators current
-        position.
-
-        If the offset puts value retrieval outside the bounds of iteration, the
-        result is unspecified.
-    **/
-    function getOffset(offset:Int):T;
+    function setTo(pos:Int):Void;
 
     /**
         Compares the RandomAccessIterator with another RandomAccessIterator
@@ -174,7 +108,7 @@ interface RandomAccessIterator<T> extends BidirectionalIterator<T> {
 
         If the RandomAccessIterator is before `it`, the result is negative. If
         `it` is before the RandomAccessIterator the result is position. If the
-        RandomAccessIterator and `it` are in the same position, the result is 0.
+        RandomAccessIterator and `it` have the same position, the result is `0`.
 
         If the RandomAccessIterator and `it` are not of the same type, or not
         iterating over the same object, the result is unspecified.
