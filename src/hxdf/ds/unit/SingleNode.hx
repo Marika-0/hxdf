60197c54c053b35cceb285ac94a6821637c9ea26
Marika <48089643+Marika-0@users.noreply.github.com>
2020-03-25 18:05:09 +1030

    Moved hxdf.ds.unit.SingleNode to its own class

--- src/hxdf/ds/unit/SingleNode.hx
+++ src/hxdf/ds/unit/SingleNode.hx
@@ -1,7 +1,5 @@
 package hxdf.ds.unit;
 
-import hxdf.ds.tuple.Double;
-
 /**
     A node containing some data and a reference to another node.
 **/
@@ -9,36 +7,19 @@ class SingleNode<T> {
     /**
         The data stored in `this` SingleNode.
     **/
-    public var data(get, set):T;
-
-    inline function get_data():T {
-        return double.first;
-    }
-
-    inline function set_data(x:T):T {
-        return double.first = x;
-    }
+    public var data:T;
 
     /**
         The node referenced by `this` SingleNode.
     **/
-    public var next(get, set):SingleNode<T>;
-
-    inline function get_next():SingleNode<T> {
-        return double.second;
-    }
-
-    inline function set_next(x:SingleNode<T>):SingleNode<T> {
-        return double.second = x;
-    }
-
-    var double:Double<T, SingleNode<T>>;
+    public var next:SingleNode<T>;
 
     /**
-        Creates a new SingleNode with given data and optional reference to
-        another SingleNode.
+        Creates a new SingleNode storing `data` with an optional reference to
+        another node `next`.
     **/
     public inline function new(data:T, ?next:SingleNode<T>) {
-        double = new Double(data, next);
+        this.data = data;
+        this.next = next;
     }
 }
