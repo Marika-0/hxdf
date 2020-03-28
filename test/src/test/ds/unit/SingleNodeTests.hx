b8be975c9549097c7b20c3e68eb2cd93b6dad765
Marika <48089643+Marika-0@users.noreply.github.com>
2020-03-28 17:46:51 +1030

    Improved hxdf.ds.unit.SingleNode tests

--- test/src/test/ds/unit/SingleNodeTests.hx
+++ test/src/test/ds/unit/SingleNodeTests.hx
@@ -4,14 +4,18 @@ import hxdf.ds.unit.SingleNode;
 
 class SingleNodeTests extends hxtf.TestObject {
     public function new() {
-        var node = new SingleNode(3, null);
+        var node = new SingleNode(3);
         node = new SingleNode(2, node);
         node = new SingleNode(1, node);
 
         var head = node;
         assert(head.data == 1);
+        assert(head.next != null);
+
         head = head.next;
         assert(head.data == 2);
+        assert(head.next != null);
+
         head = head.next;
         assert(head.data == 3);
         assert(head.next == null);
