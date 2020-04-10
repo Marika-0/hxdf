3f2aafe60bf4cf379936c8233447b0a4fabd6c15
Marika <48089643+Marika-0@users.noreply.github.com>
2020-04-10 13:23:24 +0930

    Added reflective equity test

--- src/hxdf/lambda/Compare.hx
+++ src/hxdf/lambda/Compare.hx
@@ -18,4 +18,18 @@ class Compare {
     public static inline function standardInequity<T>(a:T, b:T):Bool {
         return a != b;
     }
+
+    /**
+        Tests the equity of `a` and `b` using `Reflect.compare()` or
+        `Reflect.compareMethods()` while ensuring null-safety.
+    **/
+    public static function reflectiveEquity<T>(a:Null<T>, b:Null<T>):Bool {
+        if (a == null || b == null) {
+            return a == null && b == null;
+        }
+        if (Reflect.isFunction(a)) {
+            return Reflect.compareMethods(a, b);
+        }
+        return Reflect.compare(a, b) == 0;
+    }
 }
