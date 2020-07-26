cbc2055b361921d347b2a14dea304d9b9d6ec566
Marika <48089643+Marika-0@users.noreply.github.com>
2020-07-26 14:04:57 +0930

    Added comments.

--- src/hxdf/lambda/Convert.hx
+++ src/hxdf/lambda/Convert.hx
@@ -3,11 +3,16 @@ package hxdf.lambda;
 import hxdf.ds.Container.SequentialContainer;
 import hxdf.ds.list.DoubleLinkedList;
 import hxdf.ds.list.SingleLinkedList;
-import hxdf.ds.unit.DoubleNode;
-import hxdf.ds.unit.SingleNode;
 
+/**
+    Various conversions between datastructure types.
+**/
 class Convert {
-    @:access(hxdf.ds.list.SingleLinkedList)
+    /**
+        Converts `container` into a `hxdf.ds.list.SingleLinkedList`.
+
+        The elements of `container` are not copied and retain their identity.
+    **/
     public static function toSingleLinkedList<T>(container:SequentialContainer<T>):SingleLinkedList<T> {
         var list = new SingleLinkedList<T>();
         for (item in container) {
@@ -16,7 +21,11 @@ class Convert {
         return list;
     }
 
-    @:access(hxdf.ds.list.DoubleLinkedList)
+    /**
+        Converts `container` into a `hxdf.ds.list.DoubleLinkedList`.
+
+        The elements of `container` are not copied and retain their identity.
+    **/
     public static function toDoubleLinkedList<T>(container:SequentialContainer<T>):DoubleLinkedList<T> {
         var list = new DoubleLinkedList<T>();
         for (item in container) {
