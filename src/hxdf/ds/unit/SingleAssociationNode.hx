abd6dc0593c70653d6b63109ed672104687c0085
Marika <48089643+Marika-0@users.noreply.github.com>
2020-11-29 12:32:51 +1030

    Made various improvements to SingleAssociationNode awhile ago, forgot to commit them.

--- src/hxdf/ds/unit/SingleAssociationNode.hx
+++ src/hxdf/ds/unit/SingleAssociationNode.hx
@@ -5,18 +5,19 @@ import hxdf.ds.unit.KeyValuePair;
 /**
     A node containing a key/value pair and a reference to another node.
 **/
-class SingleAssociationNode<K, V> {
+abstract SingleAssociationNode<K, V>(SingleNode<KeyValuePair<K, V>>) from SingleNode<KeyValuePair<K, V>> to SingleNode<KeyValuePair<K, V>>
+{
     /**
         The key of `this` SingleAssociationNode.
     **/
     public var key(get, set):K;
 
     inline function get_key():K {
-        return pair.key;
+        return this.data.key;
     }
 
     inline function set_key(key:K):K {
-        return pair.key = key;
+        return this.data.key = key;
     }
 
     /**
@@ -25,29 +26,50 @@ class SingleAssociationNode<K, V> {
     public var value(get, set):V;
 
     inline function get_value():V {
-        return pair.value;
+        return this.data.value;
     }
 
     inline function set_value(value:V):V {
-        return pair.value = value;
+        return this.data.value = value;
     }
 
     /**
         The key/value pair of `this` SingleAssociationNode.
     **/
-    public var pair:KeyValuePair<K, V>;
+    public var pair(get, set):KeyValuePair<K, V>;
+
+    inline function get_pair():KeyValuePair<K, V>
+    {
+        return this.data;
+    }
+
+    inline function set_pair(pair:KeyValuePair<K, V>):KeyValuePair<K, V>
+    {
+        this.data.key = pair.key;
+        this.data.value = pair.value;
+        return this.data;
+    }
 
     /**
         The node referenced by `this` SingleAssociationNode.
     **/
-    public var next:SingleAssociationNode<K, V>;
+    public var next(get, set):SingleAssociationNode<K, V>;
+
+    inline function get_next():SingleAssociationNode<K, V>
+    {
+        return this.next;
+    }
+
+    inline function set_next(next:SingleAssociationNode<K, V>):SingleAssociationNode<K, V>
+    {
+        return this.next = next;
+    }
 
     /**
-        Creates a new SingleAssociationNode with key `key` and value `value`,
-        and an optional reference to another SingleAssociationNode.
+        Creates a new SingleAssociationNode.
     **/
-    public inline function new(key:K, value:V, ?next:SingleAssociationNode<K, V>) {
-        pair = new KeyValuePair<K, V>(key, value);
-        this.next = next;
+    public function new(key:K, value:V, ?next:SingleAssociationNode<K, V>)
+    {
+        this = new SingleNode(new KeyValuePair(key, value), next);
     }
 }
